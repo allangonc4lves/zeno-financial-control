9808fd239d0ee0e8508be9d1e8043d0b6cd9ff63
allangonc4lves <allangonc4lves@gmail.com>
2026-04-04 07:13:37 -0300

    add ROOM+KSP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,13 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
-
+    alias(libs.plugins.ksp)
 }
 
 android {
     namespace = "br.dev.allan.controlefinanceiro"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 36
+
     defaultConfig {
         applicationId = "br.dev.allan.controlefinanceiro"
         minSdk = 30
@@ -30,48 +27,46 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     buildFeatures {
         compose = true
+    }
+
+    // Inclui o código gerado pelo KSP nos sourceSets
+    sourceSets {
+        named("debug") {
+            kotlin.directories.addAll(
+                listOf("src/debug/kotlin", "build/generated/ksp/debug/kotlin"))
+        }
+        named("debug") {
+            kotlin.directories.add(
+                "src/debug/kotlin"
+            )
+        }
     }
 }
 
 dependencies {
+    // Core
     implementation(libs.androidx.core.ktx)
+
+    // Lifecycle
     implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
-
-    //Hilt
-    //implementation("com.google.dagger:hilt-android:2.59.2")
-    //ksp("com.google.dagger:hilt-compiler:2.59.2")
-    //implementation("androidx.hilt:hilt-navigation-compose:1.3.0")
-
-    // Room with coroutines
-    ////implementation("androidx.room:room-ktx:2.6.1")
-    //ksp("androidx.room:room-compiler:2.5.0")
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
 
     // Coroutines
     implementation(libs.kotlinx.coroutines.core)
 
-    // Lifecycle
-    implementation(libs.androidx.lifecycle.viewmodel.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.lifecycle.livedata.ktx)
+    // Room (com KSP)
+    implementation(libs.androidx.room.ktx)
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
 
     // Firebase (BOM + libs)
     implementation(platform(libs.firebase.bom))
@@ -84,7 +79,6 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
 
@@ -92,6 +86,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
